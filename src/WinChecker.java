1c80ca3a01b61bd2a3ca0437683bc053b1d1fec6
amankochhar <amanskochhar@gmail.com>
2016-03-14 23:28:03 -0400

    WinChecker
    Now working fine made change to checking method

--- src/WinChecker.java
+++ src/WinChecker.java
@@ -3,19 +3,20 @@ public class WinChecker extends Game{
 	
 	public static boolean Winner(String board[][], int num){
 		// checks if there is a winner or not
-		boolean winner = false;
+		boolean winner = true;
 		for(int i = 0; i < num-1; i++){
-			for(int j = 0; j<num-1; j++){
+			for(int j = 0; j < num-1; j++){
+				// checking row
 				if(board[i][j] == "." && board[i][j+1] == "." || board[num-1][j] == "." && board[num-1][j+1] == "."){
-					winner = false;
+					if(board[i][j] == "." && board[i+1][j] == "." || board[i][num-1] == "." && board[i+1][num-1] == "."){
+						winner = false;
+					}
 				}
+				// checking column
 				if(board[i][j] == "." && board[i+1][j] == "." || board[i][num-1] == "." && board[i+1][num-1] == "."){
+					if(board[i][j] == "." && board[i][j+1] == "." || board[num-1][j] == "." && board[num-1][j+1] == "."){
 					winner = false;
-				}
-				else{
-					winner = true;
-					System.out.println("We have a winner!");
-					break;
+					}
 				}
 			}
 		}
